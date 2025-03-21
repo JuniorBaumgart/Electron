637a7bb3072a4d8bf09cf0506786bec94bdd7ce6
Junior Souza <Junior de Souza>
2025-03-21 18:04:34 -0300

    Conectando com o Banco

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
-//console.log(`Processo Principal`);
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, nativeTheme } = require('electron');
+const Firebird = required('node-firebird');
+const express = require('express');
 
+const app = express();
+const port = 3000;
+
+const options= {
+    
+}
 const createWindow = () => {
+    nativeTheme.themeSource = 'light'
     const win = new BrowserWindow({
         width: 800,
-        height: 600
-    });
+        height: 600,
+        icon: './src/public/img/LogoApp.png',
+        //resizable: false,
+        //autoHideMenuBar: true,
+        //titleBarStyle: 'hidden',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
 
+    });
     win.loadFile('./src/views/index.html')
+    
 }
 
 app.whenReady().then(() => {
     createWindow()
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
 });
